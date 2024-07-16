953d98e97f60b8e6c2f94db3320c1b3af9749c0c
40VMadhumithaVM <vmadhu2711@gmail.com>
2024-07-16 15:54:59 +0530

    first commit

---
+++ src/test/java/com/example/HelloWorldJenkins/HelloWorldJenkinsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.HelloWorldJenkins;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloWorldJenkinsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
