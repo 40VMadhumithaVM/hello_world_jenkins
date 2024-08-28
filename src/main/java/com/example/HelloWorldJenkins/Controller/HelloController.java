f00c3d95da4fcd7c42938da1103677bb18f50cad
40VMadhumithaVM <vmadhu2711@gmail.com>
2024-08-28 11:23:34 +0530

    Changes

--- src/main/java/com/example/HelloWorldJenkins/Controller/HelloController.java
+++ src/main/java/com/example/HelloWorldJenkins/Controller/HelloController.java
@@ -1,13 +1,15 @@
 package com.example.HelloWorldJenkins.Controller;
 
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
+    static  int i =0;
     @GetMapping("/hello")
-    public String hello(){
+    public static String hello(){
+
         return "hello world from jenkins";
     }
-
 }
--- src/main/java/com/example/HelloWorldJenkins/HelloWorldJenkinsApplication.java
+++ src/main/java/com/example/HelloWorldJenkins/HelloWorldJenkinsApplication.java
@@ -1,12 +1,16 @@
 package com.example.HelloWorldJenkins;
 
+import com.example.HelloWorldJenkins.Controller.HelloController;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class HelloWorldJenkinsApplication {
 
+
 	public static void main(String[] args) {
+
+		System.out.println(HelloController.hello());
 		SpringApplication.run(HelloWorldJenkinsApplication.class, args);
 	}
 
